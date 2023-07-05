52c739224c1d93fb42fd145490e86b897e5f3482
RobertC91 <134195467+RobertC91@users.noreply.github.com>
2023-07-05 17:32:17 -0500

    refactor

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
@@ -30,10 +26,7 @@ setInterval(function () {
 
 // TODO: Add a listener for click events on the save button. This code should
 // use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
+// local storage.
 //
 $(document).ready(function () {
   $(".saveBtn").on("click", function () {
@@ -44,34 +37,28 @@ $(document).ready(function () {
 });
 
 // TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
+// block by comparing the id to the current hour.
 //
-let currentTime = dayjs().format("hh");
+let currentTime = dayjs().hour();
+console.log(currentTime);
 function timeWizard() {
   $(".time-block").each(function () {
     let blockTime = parseInt($(this).attr("id"));
-    if (blockTime <= currentTime) {
-      // $(this).removeClass("future");
-      // $(this).removeClass("present");
+    if (blockTime < currentTime) {
       $(this).addClass("past");
     } else if (blockTime === currentTime) {
-      // $(this).removeClass("past");
-      // $(this).removeClass("future");
       $(this).addClass("present");
     } else {
-      // $(this).removeClass("present");
-      // $(this).removeClass("past");
       $(this).addClass("future");
     }
   });
 }
+timeWizard();
+setInterval(timeWizard, 1000);
 // TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
+// the values of the corresponding textarea elements.
 //
+
 $("#9 .description").val(localStorage.getItem("9"));
 $("#10 .description").val(localStorage.getItem("10"));
 $("#11 .description").val(localStorage.getItem("11"));
@@ -81,5 +68,3 @@ $("#14 .description").val(localStorage.getItem("14"));
 $("#15 .description").val(localStorage.getItem("15"));
 $("#16 .description").val(localStorage.getItem("16"));
 $("#17 .description").val(localStorage.getItem("17"));
-
-timeWizard();
